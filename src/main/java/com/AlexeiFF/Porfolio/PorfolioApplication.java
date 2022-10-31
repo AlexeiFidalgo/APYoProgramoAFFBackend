b088370b61ace3c656f4e2e2540a196bab817ca2
Alexei F Fidalgo <alexeiffidalgo@gmail.com>
2022-10-31 20:19:36 -0300

    make it better

--- src/main/java/com/AlexeiFF/Porfolio/PorfolioApplication.java
+++ src/main/java/com/AlexeiFF/Porfolio/PorfolioApplication.java
@@ -17,7 +17,7 @@ public class PorfolioApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("**").allowedMethods("*");
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200/").allowedMethods("*");
                                 //https://apyoprogramoafffrontend.web.app
 			}
 		};
