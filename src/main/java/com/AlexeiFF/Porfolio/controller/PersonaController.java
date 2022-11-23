0826ab71fc3238a0da029a255f53801c8a82a027
Alexei F Fidalgo <alexeiffidalgo@gmail.com>
2022-11-23 02:07:52 -0300

    put persona

--- src/main/java/com/AlexeiFF/Porfolio/controller/PersonaController.java
+++ src/main/java/com/AlexeiFF/Porfolio/controller/PersonaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,7 +32,7 @@ private IUsuarioService usuarioService;
 public void crearPersona(@RequestBody Persona persona) {
        personaService.crearPersona(persona);
        }
-@PostMapping("/modificar/{id}")
+@PutMapping("/modificar/{id}")
 public void modificarPersona(@PathVariable Long id,@RequestBody Persona persona ) {
         persona.setId(id);
        personaService.crearPersona(persona);
--- src/main/java/com/AlexeiFF/Porfolio/controller/UsuarioController.java
+++ src/main/java/com/AlexeiFF/Porfolio/controller/UsuarioController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -47,7 +48,7 @@ private IProyectoService IProyectoService;
 public void crearUsuario(@RequestBody Usuario usuario) {
        usuarioService.crearUsuario(usuario);
        }
-@PostMapping("/modificar/{id}")
+@PutMapping("/modificar/{id}")
 public void modificar(@PathVariable Long id,@RequestBody  Usuario usuario) {
        usuarioService.crearUsuario(usuario);
        }
